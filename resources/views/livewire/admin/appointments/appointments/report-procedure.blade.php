7532bd2ea4e58d3e2efb76018056617b6f8e5d32
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-08-12 12:57:33 -0300

    Refactor report views to utilize a base report component for improved consistency and maintainability

--- resources/views/livewire/admin/appointments/appointments/report-procedure.blade.php
+++ resources/views/livewire/admin/appointments/appointments/report-procedure.blade.php
@@ -1,40 +1,16 @@
-@php use App\Enums\Models\Report\Status; @endphp
-<x-modal size="4xl" :title="__('Procedure report')" wire>
-    <div class="space-y-4">
-        <form id="report-{{ $id = str()->uuid() }}" wire:submit="save" class="space-y-6">
-            <div class="space-y-3">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-4 bg-gray-50 p-4 rounded-lg">
-                    <x-ui.date wire:model="date_start" required :label="__('Start Date')" />
-                    <x-ui.date wire:model="date_end" required :label="__('End Date')" />
-                </div>
+<x-admin.report.base-report title="Procedure report" view="pdf.report.procedure">
+    <div class="space-y-3">
+        <div class="bg-gray-50 p-4 rounded-lg space-y-4">
+            <div class="gap-4 grid grid-cols-1 md:grid-cols-2">
+
+                <x-select.procedure
+                    wire:model.live="procedure_id"
+                />
+                <x-select.employee
+                    wire:model.live="employee_id"
+                    :label="__('Doctor')"
+                />
             </div>
-
-            <div class="space-y-3">
-                <div class="bg-gray-50 p-4 rounded-lg space-y-4">
-                    <div class="gap-4 grid grid-cols-1 md:grid-cols-2">
-
-                        <x-select.procedure
-                            wire:model.live="procedure_id"
-                        />
-                        <x-select.employee
-                            wire:model.live="employee_id"
-                            :label="__('Doctor')"
-                        />
-                    </div>
-                </div>
-            </div>
-        </form>
-
-        <livewire:admin.report.status :$report wire:key="report-{{ $report?->id }}" />
-
-        @if($modal)
-            <livewire:admin.report.report view="pdf.report.procedure" />
-        @endif
+        </div>
     </div>
-
-    <x-slot:footer>
-        <x-button type="submit" form="report-{{ $id }}">
-            @lang('Generate PDF Report')
-        </x-button>
-    </x-slot:footer>
-</x-modal>
+</x-admin.report.base-report>
--- resources/views/livewire/admin/appointments/appointments/report-schedule.blade.php
+++ resources/views/livewire/admin/appointments/appointments/report-schedule.blade.php
@@ -1,60 +1,36 @@
-@php use App\Enums\Models\Report\Status; @endphp
-<x-modal size="4xl" :title="__('Scheduling report')" wire>
-    <div class="space-y-4">
-        <form id="report-{{ $id = str()->uuid() }}" wire:submit="save" class="space-y-6">
-            <div class="space-y-3">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-4 bg-gray-50 p-4 rounded-lg">
-                    <x-ui.date wire:model="date_start" required :label="__('Start Date')" />
-                    <x-ui.date wire:model="date_end" required :label="__('End Date')" />
-                </div>
+<x-admin.report.base-report title="Scheduling report" view="pdf.report.schedule">
+    <div class="space-y-3">
+        <div class="bg-gray-50 p-4 rounded-lg space-y-4">
+            <div class="gap-4 grid grid-cols-1 md:grid-cols-2">
+                <x-select.styled
+                    :placeholders="['default' => __('All')]"
+                    :options="$this->status()"
+                    select="label:name|value:value"
+                    :label="__('Status')"
+                    wire:model="status"
+                />
+                <x-select.styled
+                    :placeholders="['default' => __('All')]"
+                    :options="$this->payed()"
+                    select="label:name|value:value"
+                    :label="__('Payed')"
+                    wire:model="is_payed"
+                />
             </div>
 
-            <div class="space-y-3">
-                <div class="bg-gray-50 p-4 rounded-lg space-y-4">
-                    <div class="gap-4 grid grid-cols-1 md:grid-cols-2">
-                        <x-select.styled
-                            :placeholders="['default' => __('All')]"
-                            :options="$this->status()"
-                            select="label:name|value:value"
-                            :label="__('Status')"
-                            wire:model="status"
-                        />
-                        <x-select.styled
-                            :placeholders="['default' => __('All')]"
-                            :options="$this->payed()"
-                            select="label:name|value:value"
-                            :label="__('Payed')"
-                            wire:model="is_payed"
-                        />
-                    </div>
-
-                    <div class="gap-4 grid grid-cols-1 md:grid-cols-3">
-                        <x-select.agreement
-                            :is-particular="true"
-                            wire:model="agreement_id"
-                        />
-                        <x-select.procedure
-                            wire:model.live="procedure_id"
-                        />
-                        <x-select.employee
-                            wire:model.live="employee_id"
-                            :label="__('Doctor')"
-                        />
-                    </div>
-                </div>
+            <div class="gap-4 grid grid-cols-1 md:grid-cols-3">
+                <x-select.agreement
+                    :is-particular="true"
+                    wire:model="agreement_id"
+                />
+                <x-select.procedure
+                    wire:model.live="procedure_id"
+                />
+                <x-select.employee
+                    wire:model.live="employee_id"
+                    :label="__('Doctor')"
+                />
             </div>
-        </form>
-
-        <livewire:admin.report.status :$report wire:key="report-{{ $report?->id }}" />
-
-        @if($modal)
-            <livewire:admin.report.report view="pdf.report.schedule" />
-        @endif
+        </div>
     </div>
-
-    <x-slot:footer>
-        <x-button type="submit" form="report-{{ $id }}">
-            @lang('Generate PDF Report')
-        </x-button>
-    </x-slot:footer>
-</x-modal>
+</x-admin.report.base-report>
