ed39a163bac8636aed65a28bbe0182ed922bc652
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-08-12 19:04:59 -0300

    Update console commands to schedule daily pruning for Telescope and queue batches

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,7 @@
 
 declare(strict_types = 1);
 
-use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function (): void {
-    /* @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('telescope:prune')->daily();
+Schedule::command(sprintf('queue:prune-batches --hours=%s --unfinished=%s --cancelled=%s', 48, 72, 72))->daily();
