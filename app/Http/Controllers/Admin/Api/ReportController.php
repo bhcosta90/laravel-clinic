8402633c69f2bec2d3f52b261825f41b61a24296
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-08-12 19:00:44 -0300

    Refactor report file retrieval route and authorization logic; update view-file method to include auditing for non-owners

--- app/Http/Controllers/Admin/Api/ReportController.php
+++ app/Http/Controllers/Admin/Api/ReportController.php
@@ -6,7 +6,9 @@ namespace App\Http\Controllers\Admin\Api;
 
 use App\Models\Report;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Storage;
+use OwenIt\Auditing\Events\AuditCustom;
 
 final class ReportController
 {
@@ -14,11 +16,17 @@ final class ReportController
 
     public function viewFile(Report $report)
     {
-        $this->authorize('show-file', $report);
+        when(auth()->check() || !$report->can_shared, fn () => $this->authorize('show-file', $report));
 
         $content = Storage::drive($report->filesystem)->get($report->file);
 
         $name = sprintf('%s-%s.%s', str()->slug($report->name), now()->timestamp, 'pdf');
+
+        if (!auth()->check() || auth()->id() !== $report->user_id) {
+            $report->auditEvent    = 'open-file';
+            $report->isCustomEvent = true;
+            Event::dispatch(new AuditCustom($report));
+        }
 
         return response($content, 200)
             ->header('Content-Type', 'application/pdf')
--- routes/web.php
+++ routes/web.php
@@ -8,32 +8,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
 
-Route::middleware(['auth', ImpersonateMiddleware::class])->as('admin.')->prefix('admin')->group(function (): void {
-    Route::view('/dashboard', 'dashboard')->name('dashboard');
+Route::middleware([ImpersonateMiddleware::class])->group(function () {
+    Route::get('report/{report:code}/view-file', [Api\ReportController::class, 'viewFile'])->name('report.view-file');
 
-    if (file_exists(base_path('routes/admin/testIgnore.php'))) {
-        Route::prefix('test')->as('test.')->group(base_path('routes/admin/testIgnore.php'));
-    }
+    Route::middleware(['auth', ImpersonateMiddleware::class])->as('admin.')->prefix('admin')->group(function (): void {
+        Route::view('/dashboard', 'dashboard')->name('dashboard');
 
-    Route::prefix('v1')->name('v1.')->group(function (): void {
-        Route::as('people.')->prefix('people')->group(base_path('routes/admin/v1/people.php'));
-        Route::as('registration.')->prefix('registration')->group(base_path('routes/admin/v1/registration.php'));
-        Route::as('transactions.')->prefix('transaction')->group(base_path('routes/admin/v1/transaction.php'));
-        Route::as('appointments.')->prefix('appointment')->group(base_path('routes/admin/v1/appointment.php'));
+        if (file_exists(base_path('routes/admin/testIgnore.php'))) {
+            Route::prefix('test')->as('test.')->group(base_path('routes/admin/testIgnore.php'));
+        }
 
-        Route::get('report/{report:code}/view-file', [Api\ReportController::class, 'viewFile'])->name('report.view-file');
+        Route::prefix('v1')->name('v1.')->group(function (): void {
+            Route::as('people.')->prefix('people')->group(base_path('routes/admin/v1/people.php'));
+            Route::as('registration.')->prefix('registration')->group(base_path('routes/admin/v1/registration.php'));
+            Route::as('transactions.')->prefix('transaction')->group(base_path('routes/admin/v1/transaction.php'));
+            Route::as('appointments.')->prefix('appointment')->group(base_path('routes/admin/v1/appointment.php'));
 
-        Route::prefix('api')->as('api.')->group(function (): void {
-            Route::get('roles/search', [Api\RoleController::class, 'search'])->name('roles.search');
-            Route::get('agreements/search', [Api\AgreementController::class, 'search'])->name('agreement.search');
-            Route::get('anamnesis-group/search', [Api\AnamnesisGroupController::class, 'search'])->name('anamnesis-group.search');
-            Route::get('permissions/search', [Api\PermissionController::class, 'search'])->name('permission.search');
-            Route::get('customers/search', [Api\CustomerController::class, 'search'])->name('customer.search');
-            Route::get('procedures/search', [Api\ProcedureController::class, 'search'])->name('procedure.search');
-            Route::get('payment-methods/search', [Api\PaymentMethodController::class, 'search'])->name('payment-method.search');
-            Route::get('users/search', [Api\UserController::class, 'search'])->name('user.search');
+            Route::prefix('api')->as('api.')->group(function (): void {
+                Route::get('roles/search', [Api\RoleController::class, 'search'])->name('roles.search');
+                Route::get('agreements/search', [Api\AgreementController::class, 'search'])->name('agreement.search');
+                Route::get('anamnesis-group/search', [Api\AnamnesisGroupController::class, 'search'])->name('anamnesis-group.search');
+                Route::get('permissions/search', [Api\PermissionController::class, 'search'])->name('permission.search');
+                Route::get('customers/search', [Api\CustomerController::class, 'search'])->name('customer.search');
+                Route::get('procedures/search', [Api\ProcedureController::class, 'search'])->name('procedure.search');
+                Route::get('payment-methods/search', [Api\PaymentMethodController::class, 'search'])->name('payment-method.search');
+                Route::get('users/search', [Api\UserController::class, 'search'])->name('user.search');
+            });
         });
     });
-});
 
-require __DIR__ . '/auth.php';
+    require __DIR__ . '/auth.php';
+});
