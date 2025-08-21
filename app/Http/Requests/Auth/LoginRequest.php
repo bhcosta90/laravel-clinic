ee5725218d6b3914eca477e9f09379ed1ff223ee
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-08-21 18:42:19 -0300

    Add UserService and HandlesWithDependencies trait for improved service handling

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -4,9 +4,9 @@ declare(strict_types = 1);
 
 namespace App\Http\Requests\Auth;
 
+use App\Services\UserService;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
@@ -30,7 +30,7 @@ final class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (!Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (!app(UserService::class)->handle('login', $this->email, $this->password, $this->remember)) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages(['email' => trans('auth.failed')]);
---
+++ app/Services/UserService.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Traits\Services\HandlesWithDependencies;
+use Illuminate\Container\Attributes\CurrentUser;
+use Illuminate\Support\Facades\Auth;
+use QuantumTecnology\ControllerBasicsExtension\Builder\BuilderQuery;
+
+final class UserService
+{
+    use HandlesWithDependencies;
+
+    protected function login(string $username, string $password, bool $remember = false): bool
+    {
+        return Auth::attempt([
+            'email'    => $username,
+            'password' => $password,
+        ], $remember);
+    }
+
+    protected function index(#[CurrentUser] $user, ?string $search)
+    {
+        return app(BuilderQuery::class)->execute(new User(), [
+            'role' => ['name'],
+        ], [
+            '(byFilter,name;email)' => $search,
+        ])
+            ->where('id', '!=', $user->id);
+    }
+}
