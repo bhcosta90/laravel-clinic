339f6da28f9173937309f720b98b0976221bddf3
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-08-25 18:03:28 -0300

    feat: add TriageSeeder for populating triage data with random customer associations

---
+++ database/seeders/TriageSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use App\Models\Triage;
+use Illuminate\Database\Seeder;
+
+final class TriageSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $customers = Customer::pluck('id')->toArray();
+        Triage::factory(25)->make()->each(function ($item) use ($customers) {
+            $item->customer_id = collect($customers)->random();
+            $item->save();
+        });
+
+    }
+}
