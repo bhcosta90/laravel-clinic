4fa7057679d225cca35cd531e37669f9b313e2f4
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-08-27 13:55:03 -0300

    feat: update TriageFactory to use RiskClassification enum for risk classification

--- database/factories/TriageFactory.php
+++ database/factories/TriageFactory.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Database\Factories;
 
+use App\Enums\Models\Triage\RiskClassification;
 use App\Models\Customer;
 use App\Models\Triage;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -16,22 +17,22 @@ final class TriageFactory extends Factory
     public function definition(): array
     {
         return [
-            'customer_id'        => Customer::factory(),
-            'risk'               => $this->faker->randomNumber(1, 5),
-            'description'        => $this->faker->sentence(3),
-            'mmhg'               => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'bpm'                => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'irpm'               => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'temperature'        => $this->faker->numberBetween(20, 32),
-            'saturation'         => $this->faker->numberBetween(92, 98),
-            'allergies'          => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'current_medication' => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'history_diseases'   => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'time_symptom_onset' => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'general_condition'  => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
-            'eva'                => $this->faker->randomNumber(1, 10),
-            'created_at'         => Carbon::now(),
-            'updated_at'         => Carbon::now(),
+            'customer_id'         => Customer::factory(),
+            'risk_classification' => $this->faker->randomElement(RiskClassification::cases()),
+            'description'         => $this->faker->sentence(3),
+            'mmhg'                => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'bpm'                 => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'irpm'                => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'temperature'         => $this->faker->numberBetween(20, 32),
+            'saturation'          => $this->faker->numberBetween(92, 98),
+            'allergies'           => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'current_medication'  => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'history_diseases'    => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'time_symptom_onset'  => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'general_condition'   => when($this->faker->boolean(), fn () => $this->faker->sentence(2)),
+            'eva'                 => $this->faker->randomNumber(1, 10),
+            'created_at'          => Carbon::now(),
+            'updated_at'          => Carbon::now(),
         ];
     }
 }
