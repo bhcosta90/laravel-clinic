0be9cd7fdc75d864b30d88998284eb265a6f8944
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-01 18:22:07 -0300

    refactor: improve formatting of cache key construction in ClearCacheObserver

--- app/Observers/ClearCacheObserver.php
+++ app/Observers/ClearCacheObserver.php
@@ -15,7 +15,8 @@ final class ClearCacheObserver
     public function saved(Role | User $model): void
     {
         $keyId = $model->getKeyName();
-        $key   = config('cache.times.permission_prefix') . $model->getTable() . ".{$model->{$keyId}}.permissions";
+
+        $key = config('cache.times.permission_prefix') . $model->getTable() . ".{$model->{$keyId}}.permissions";
         Cache::purge($key);
         Cache::forget($key);
     }
