fffa82200ae8486fb423a5be8918d27e30990b0d
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-02 13:59:22 -0300

    feat: add Create, Delete, Index, and Update components for Location management

---
+++ app/Livewire/Admin/Stock/Location/Update.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Livewire\Admin\Stock\Location;
+
+use App\Livewire\Traits\Alert;
+use App\Models\Frequency;
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+final class Update extends Component
+{
+    use Alert;
+
+    public Form $form;
+
+    public bool $modal = false;
+
+    public function render(): View
+    {
+        return view('livewire.admin.stock.location.update');
+    }
+
+    #[On('load::frequency')]
+    public function load(Frequency $frequency): void
+    {
+        $this->form->setModel($frequency);
+        $this->modal = true;
+    }
+
+    public function save(): void
+    {
+        $model = $this->form->save();
+
+        $this->dispatch('updated');
+
+        $this->form->setModel($model);
+
+        $this->success();
+        $this->resetExcept('form');
+    }
+}
