5c5d4710417d6f14481e27799411dda33e757cd0
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-02 17:14:37 -0300

    refactor: remove unused model imports and attributes from AppointmentFactory

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -5,8 +5,6 @@ declare(strict_types = 1);
 namespace Database\Factories;
 
 use App\Models\Appointment;
-use App\Models\Customer;
-use App\Models\Procedure;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
@@ -19,8 +17,6 @@ final class AppointmentFactory extends Factory
     {
         return [
             'tenant_id'            => DatabaseSeeder::TenantId,
-            'customer_id'          => Customer::factory(),
-            'procedure_id'         => Procedure::factory(),
             'date'                 => $this->faker->dateTimeBetween(now()->firstOfMonth(), now()->endOfMonth()),
             'is_return'            => $this->faker->boolean(),
             'exam_withdrawal_date' => $this->faker->sentence(2),
