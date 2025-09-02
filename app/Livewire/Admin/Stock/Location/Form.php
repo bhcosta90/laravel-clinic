5cce4d0fbc2ac6fb063d478bb7559d741181725d
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-02 13:59:29 -0300

    feat: add Form component for managing Frequency model with validation and save functionality

---
+++ app/Livewire/Admin/Stock/Location/Form.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Livewire\Admin\Stock\Location;
+
+use App\Models\Frequency;
+use App\Services\FrequencyService;
+
+final class Form extends \Livewire\Form
+{
+    public ?Frequency $model = null;
+
+    public $name;
+    public $days;
+
+    public function setModel(Frequency $model): void
+    {
+        $this->model = $model;
+        $this->name  = $model->name;
+        $this->days  = $model->days;
+    }
+
+    public function save(): Frequency
+    {
+        $data = $this->validate();
+
+        if ($this->model?->id) {
+            app(FrequencyService::class)->handle('update', $this->model, $data);
+
+            return $this->model;
+        }
+
+        return app(FrequencyService::class)->handle('store', $data);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'days' => ['required', 'numeric', 'min:0'],
+        ];
+    }
+}
