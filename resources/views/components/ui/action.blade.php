9efcc511393db27d8b029e2ff9b2c405b169f598
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-05 18:43:20 -0300

    feat: add footer slot to slide component and fix model ID reference in update form

--- resources/views/components/ui/action.blade.php
+++ resources/views/components/ui/action.blade.php
@@ -1,3 +1,7 @@
 <x-slide {{$attributes}} wire="modal">
     {{ $slot }}
+
+    <x-slot:footer>
+        {{ $footer }}
+    </x-slot:footer>
 </x-slide>
--- resources/views/livewire/admin/registration/agreements/update.blade.php
+++ resources/views/livewire/admin/registration/agreements/update.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($modal)
-        <x-ui.action size="3xl" :title="__('Update Agreement: #:id', ['id' => $form->model])">
+        <x-ui.action size="3xl" :title="__('Update Agreement: #:id', ['id' => $form->model->id])">
             <form id="agreement-update" wire:submit="save" class="space-y-4">
                 <x-admin.registration.agreements.form />
             </form>
