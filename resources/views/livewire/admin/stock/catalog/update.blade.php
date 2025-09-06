a7df8df40b6e4cc56377005c9f4e6b8fa703f270
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-06 11:35:58 -0300

    feat: remove unnecessary updated prop from catalog form component

--- resources/views/livewire/admin/stock/catalog/update.blade.php
+++ resources/views/livewire/admin/stock/catalog/update.blade.php
@@ -37,7 +37,7 @@
             </x-slot:header>
 
             <form id="catalog-update-{{ $this->form->model->hash_code }}" wire:submit="save" class="space-y-4">
-                <x-admin.stock.catalog.form :updated="true" />
+                <x-admin.stock.catalog.form />
             </form>
         </x-card>
     </div>
