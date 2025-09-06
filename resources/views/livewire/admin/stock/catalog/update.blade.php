f6fea7c21904a0cafe0332641551f2398bdff1ef
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-06 10:15:17 -0300

    feat: restructure catalog update form layout for improved organization

--- resources/views/livewire/admin/stock/catalog/update.blade.php
+++ resources/views/livewire/admin/stock/catalog/update.blade.php
@@ -1,19 +1,22 @@
 <div class="space-y-3">
-    <x-card>
-        <x-slot:header>
-            <x-ui.header :title="__('Catalog')">
-                <x-slot name="actions">
-                    <x-button type="submit" form="catalog-update-{{ $this->form->model->hash_code }}">
-                        @lang('Save')
-                    </x-button>
-                </x-slot>
-            </x-ui.header>
-        </x-slot:header>
+    <div class="block-1">
+        <x-card>
+            <x-slot:header>
+                <x-ui.header :title="__('Catalog')">
+                    <x-slot name="actions">
+                        <x-button type="submit" form="catalog-update-{{ $this->form->model->hash_code }}">
+                            @lang('Save')
+                        </x-button>
+                    </x-slot>
+                </x-ui.header>
+            </x-slot:header>
 
-        <form id="catalog-update-{{ $this->form->model->hash_code }}" wire:submit="save" class="space-y-4">
-            <x-admin.stock.catalog.form :updated="true" />
-        </form>
-    </x-card>
-
-    <livewire:admin.stock.packing.index :model="$this->form->model" />
+            <form id="catalog-update-{{ $this->form->model->hash_code }}" wire:submit="save" class="space-y-4">
+                <x-admin.stock.catalog.form :updated="true" />
+            </form>
+        </x-card>
+    </div>
+    <div class="block-2">
+        <livewire:admin.stock.packing.index :model="$this->form->model" />
+    </div>
 </div>
