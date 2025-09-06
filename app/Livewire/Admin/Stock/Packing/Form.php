dd151c28923e75c1873dd02ee105a52a6f8e0b50
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-06 12:29:23 -0300

    feat: update form to include packing details with level, quantity, and dimensions

--- app/Livewire/Admin/Stock/Packing/Form.php
+++ app/Livewire/Admin/Stock/Packing/Form.php
@@ -11,12 +11,22 @@ final class Form extends \Livewire\Form
 {
     public ?Packing $model = null;
 
-    public $name;
+    public $level;
+    public $quantity;
+    public $weight;
+    public $length;
+    public $width;
+    public $height;
 
     public function setModel(Packing $model): void
     {
-        $this->model = $model;
-        $this->name  = $model->name;
+        $this->model    = $model;
+        $this->level    = $model->level;
+        $this->quantity = $model->quantity;
+        $this->weight   = $model->weight;
+        $this->length   = $model->length;
+        $this->width    = $model->width;
+        $this->height   = $model->height;
     }
 
     public function save(): Packing
--- resources/views/components/admin/stock/packing/form.blade.php
+++ resources/views/components/admin/stock/packing/form.blade.php
@@ -1,4 +1,11 @@
+@php use App\Enums\Models\Packing\Level; @endphp
 <div class="space-y-4">
-    <x-input label="{{ __('Name') }} *" wire:model="form.name" required />
+    <x-ui.select.enum :enum="Level::class" label="{{ __('Level') }} *" wire:model="form.level" required />
+    <x-ui.number label="{{ __('Quantity') }} *" wire:model="form.quantity" required />
+    <x-ui.number label="{{ __('Weight') }} *" wire:model="form.weight" required />
+    <x-ui.number label="{{ __('Length') }} *" wire:model="form.length" required />
+    <x-ui.number label="{{ __('Width') }} *" wire:model="form.width" required />
+    <x-ui.number label="{{ __('Height') }} *" wire:model="form.height" required />
+
     <x-message.required />
 </div>
