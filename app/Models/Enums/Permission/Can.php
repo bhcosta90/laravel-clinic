0d3a0f04155748431768243ad216c76b5cefac2c
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-15 13:21:03 -0300

    Refactor Can enum to add comprehensive permission cases for various models

--- app/Models/Enums/Permission/Can.php
+++ app/Models/Enums/Permission/Can.php
@@ -4,6 +4,55 @@ declare(strict_types = 1);
 
 namespace App\Models\Enums\Permission;
 
-enum Can
+enum Can: string
 {
+    case PeoplePatientView           = 'people::patient::view';
+    case PeoplePatientEdit           = 'people::patient::edit';
+    case PeoplePatientBirthdayReport = 'people::patient birthday::report';
+    case PeopleUserView              = 'people::user::view';
+    case PeopleUserEdit              = 'people::user::edit';
+    case PeopleUserImpersonate       = 'people::user::impersonate';
+    case PeopleEmployeeView          = 'people::employee::view';
+    case PeopleEmployeeEdit          = 'people::employee::edit';
+
+    case RegistrationRoleView           = 'registration::role::view';
+    case RegistrationRoleEdit           = 'registration::role::edit';
+    case RegistrationProcedureView      = 'registration::procedure::view';
+    case RegistrationProcedureReport    = 'registration::procedure::report';
+    case RegistrationProcedureEdit      = 'registration::procedure::edit';
+    case RegistrationAgreementsView     = 'registration::agreements::view';
+    case RegistrationAgreementsEdit     = 'registration::agreements::edit';
+    case RegistrationPaymentMethodsView = 'registration::paymentsMethods::view';
+    case RegistrationPaymentMethodsEdit = 'registration::paymentsMethods::edit';
+    case RegistrationFrequencyView      = 'registration::frequency::view';
+    case RegistrationFrequencyEdit      = 'registration::frequency::edit';
+    case RegistrationRemedyView         = 'registration::remedy::view';
+    case RegistrationRemedyEdit         = 'registration::remedy::edit';
+    case RegistrationRoomView           = 'registration::room::view';
+    case RegistrationRoomEdit           = 'registration::room::edit';
+    case TriageView                     = 'triage::view';
+    case TriageEdit                     = 'triage::edit';
+    case RegistrationAnamnesisGroupView = 'registration::anamnesis_group::view';
+    case RegistrationAnamnesisGroupEdit = 'registration::anamnesis_group::edit';
+    case RegistrationAnamnesisTypeView  = 'registration::anamnesis_type::view';
+    case RegistrationAnamnesisTypeEdit  = 'registration::anamnesis_type::edit';
+    case TransactionIncomeView          = 'transaction::income::view';
+    case TransactionIncomeEdit          = 'transaction::income::edit';
+
+    case TransactionExpenseView       = 'transaction::expense::view';
+    case TransactionExpenseEdit       = 'transaction::expense::edit';
+    case TransactionCommissionView    = 'transaction::commission::view';
+    case TransactionCommissionEdit    = 'transaction::commission::edit';
+    case AppointmentAppointmentReport = 'appointment::appointment::report';
+    case AppointmentAppointmentView   = 'appointment::appointment::view';
+    case AppointmentAppointmentEdit   = 'appointment::appointment::edit';
+    case StockLocationImportView      = 'stock::location_import::view';
+    case StockLocationImportEdit      = 'stock::location_import::edit';
+    case StockLocationModuleView      = 'stock::location_module::view';
+    case StockLocationModuleEdit      = 'stock::location_module::edit';
+    case StockSectorView              = 'stock::sector::view';
+    case StockSectorEdit              = 'stock::sector::edit';
+    case StockCatalogView             = 'stock::catalog::view';
+    case StockCatalogEdit             = 'stock::catalog::edit';
+
 }
