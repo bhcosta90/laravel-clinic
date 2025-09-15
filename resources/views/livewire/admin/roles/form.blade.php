3632e88b9680dfb32e20e8ca93efcd0e57b34bd9
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-15 15:00:17 -0300

    Remove unused fields from role form in Admin Livewire component

--- resources/views/livewire/admin/roles/form.blade.php
+++ resources/views/livewire/admin/roles/form.blade.php
@@ -4,8 +4,6 @@
     @endif
 
     <x-ui.slide.form :model="$model" title="role">
-        <x-ui.input wire:model="model.code" label="Code" placeholder="Enter role code" />
         <x-ui.input wire:model="model.name" label="Name" placeholder="Enter role name" />
-        <x-ui.enable wire:model="model.is_active" label="Status" placeholder="Select role status" />
     </x-ui.slide.form>
 </div>
