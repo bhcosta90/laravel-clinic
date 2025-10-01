6a54914ee2282eff8cfeec6f336274f1a44418fd
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-10-01 11:04:10 -0300

    Refactor DoctorScheduleOutput class to use 'readonly' modifier for improved immutability

--- core/Application/Data/DoctorScheduleOutput.php
+++ core/Application/Data/DoctorScheduleOutput.php
@@ -4,15 +4,15 @@ declare(strict_types = 1);
 
 namespace Core\Application\Data;
 
-final readonly class DoctorScheduleOutput
+final class DoctorScheduleOutput
 {
     public function __construct(
-        public int | string $id,
-        public int | string $doctor_id,
+        public readonly int | string $id,
+        public readonly int | string $doctor_id,
         public string $day_of_week,
-        public string $start_time,
-        public string $end_time,
-        public int $slot_minutes,
+        public readonly string $start_time,
+        public readonly string $end_time,
+        public readonly int $slot_minutes,
     ) {
         $this->day_of_week = mb_strtolower($this->day_of_week);
     }
