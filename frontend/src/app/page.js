8e3d25ef80c105c0b713dc0852238807f927a052
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-10-02 15:53:43 -0300

    Update Select component usage to set valueField for improved data handling

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,7 @@
-"use client";
 import Image from "next/image";
 import Select from "@/components/Select";
-import {useState} from "react";
 
 export default function Home() {
-    const [selectedCategory, setSelectedCategory] = useState(null);
 
     return (
     <div>
@@ -12,6 +9,7 @@ export default function Home() {
             apiUrl="http://127.0.0.1:8600/api/v1/procedures?fields=uuid name"
             onSelect={() => {}}
             labelField='data.name'
+            valueField='data.uuid'
             //renderItem={(u) => <div>{u.data.name} ({u.data.name})</div>}
         />
     </div>
